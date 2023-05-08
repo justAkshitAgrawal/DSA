342924264f2e082e0e3aecdebd091d5a727e08ea
Akshit Agrawal <71216807+justAkshitAgrawal@users.noreply.github.com>
2023-05-09 00:08:18 +0530

    S

--- reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
+++ reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -1,20 +1,20 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
-        
         ListNode temp = head, curr = head, prev = null;
 
         for (int i = 0; i < k; i++, temp = temp.next) {
-            if (temp == null) return head;
+            if (temp == null)
+                return head;
         }
 
         for (int i = 0; i < k; i++) {
